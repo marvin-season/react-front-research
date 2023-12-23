42e8a41ff423fa779f56339a071d7b00ea2f2e4b
marvin <marvin010528@foxmail.com>
2023-12-23 17:06:07 +0800

    layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,15 +1,41 @@
 import {Outlet, useNavigate} from "react-router-dom";
+import styled from 'styled-components'
+import {useState} from "react";
+
+const LayoutContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  height: 100vh;
+  overflow: auto;
+`
+
+const NavItem = styled.div<{ activated: boolean }>`
+  cursor: pointer;
+  width: 200px;
+  padding: 4px;
+  border-radius: 8px;
+  font-size: 16px;
+
+  color: ${({activated}) => activated ? 'blue' : 'black'};
+`
+
+const Nav = styled.div`
+  padding: 10px
+`
 
 const Layout = () => {
     const navigate = useNavigate();
+    const [currentIndex, setCurrentIndex] = useState(0)
     const navs = [
         {
+            index: 0,
             label: '首页',
             onClick: () => {
                 navigate('/index');
             }
         },
         {
+            index: 1,
             label: '文件预览',
             onClick: () => {
                 navigate('/file-preview');
@@ -17,19 +43,24 @@ const Layout = () => {
         },
     ]
     return <>
-        <div style={{
-            display: "flex",
-            justifyContent: "flex-start"
-        }}>
-            <div style={{width: '200px'}}>
+        <LayoutContainer>
+            <Nav>
                 {
-                    navs.map((item) => <div key={item.label} onClick={item.onClick}>{item.label}</div>)
+                    navs.map(({index, onClick, label}) =>
+                        <NavItem
+                            key={index}
+                            activated={currentIndex == index}
+                            onClick={() => {
+                                setCurrentIndex(index)
+                                onClick()
+                            }}>{label}
+                        </NavItem>)
                 }
-            </div>
+            </Nav>
             <div style={{flex: 1}}>
                 <Outlet/>
             </div>
-        </div>
+        </LayoutContainer>
     </>
 }
 
